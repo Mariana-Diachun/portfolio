a91b924ee993f56b33abbd5b55efc5ec0e932265
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-07-09 22:09:10 +0100

    rename classes and remove information in section about

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // -----------typing animation------------
 let typed = new Typed(".typing", {
-  strings: ["", "Software Developer"],
+  strings: ["", "Software Developer", "Software Developer"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true,
