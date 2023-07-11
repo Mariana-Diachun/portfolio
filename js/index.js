b5eaab0b9ad2f629ae1cd0ca4cdb101a30588f7d
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-07-11 09:54:17 +0100

    add current link

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
-// -----------typing animation------------
-let typed = new Typed(".typing", {
-  strings: ["", "Software Developer", "Software Developer"],
-  typeSpeed: 100,
-  BackSpeed: 60,
-  loop: true,
-})
+// // -----------typing animation------------
+// let typed = new Typed(".typing", {
+//   strings: ["", "Software Developer", "Software Developer"],
+//   typeSpeed: 100,
+//   BackSpeed: 60,
+//   loop: true,
+// })
 
-// active link
+// ----------------active link-----------------------
+const linkNavRef = document.getElementsByClassName("nav-link")
 
-const activeLink = document.querySelector(".nav")
+for (let i = 0; i < linkNavRef.length; i++) {
+  linkNavRef[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active")
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "")
+    }
+    this.className += " active"
+  })
+}
